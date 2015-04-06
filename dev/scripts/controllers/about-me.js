d0345d2afdafaa175b0c28ebc04e15218dc4bd4d
Nghi Le Vinh <vinhnghi223@gmail.com>
2015-04-06 21:22:12 +0300

    fix on avt click + add dtfApp

--- dev/scripts/controllers/about-me.js
+++ dev/scripts/controllers/about-me.js
@@ -2,7 +2,7 @@ app.controller('about-me', ['$scope', function($scope) {
   var s = skrollr.init();
   $("#avt").click(function() {
       $('html, body').animate({
-          scrollTop: $("#slide-2 .caption").offset().top+300
+          scrollTop: 310
       }, 400);
   });
 
@@ -15,34 +15,34 @@ app.controller('about-me', ['$scope', function($scope) {
   
   //Animation while scrolling
   $(window).scroll(function() {
-    console.log("Width: "+$(window).width());
-      //console.log($(this).scrollTop());
-      //Slide 1
-      if ($(this).scrollTop()>260){
-          $('#slide-1 #intro').fadeOut();
-       }else{
-        $('#slide-1 #intro').fadeIn();
-      }
+    //console.log("Width: "+$(window).width());
+    //console.log($(this).scrollTop());
+    //Slide 1
+    if ($(this).scrollTop()>260){
+        $('#slide-1 #intro').fadeOut();
+     }else{
+      $('#slide-1 #intro').fadeIn();
+    }
 
-      //Slide 2
-      if ($(this).scrollTop()>=300 && $(this).scrollTop()<800){
-          $('#slide-2 .caption.part1').fadeIn();
-      }else{     
-          $('#slide-2 .caption.part1').fadeOut();
-      }
+    //Slide 2
+    if ($(this).scrollTop()>=300 && $(this).scrollTop()<800){
+        $('#slide-2 .caption.part1').fadeIn();
+    }else{     
+        $('#slide-2 .caption.part1').fadeOut();
+    }
 
-      if ($(this).scrollTop()>=800 && $(this).scrollTop()<1300){
-          $('#slide-2 .caption.part2').fadeIn();
-      }else{     
-          $('#slide-2 .caption.part2').fadeOut();
-      }      
+    if ($(this).scrollTop()>=800 && $(this).scrollTop()<1300){
+        $('#slide-2 .caption.part2').fadeIn();
+    }else{     
+        $('#slide-2 .caption.part2').fadeOut();
+    }      
 
-      //Slide 3
-      if ($(this).scrollTop()>1450 && $(this).scrollTop()<2300){
-          $('#slide-3 .caption').fadeIn();
-      }else{     
-        $('#slide-3 .caption').fadeOut();
-      }
+    //Slide 3
+    if ($(this).scrollTop()>1450 && $(this).scrollTop()<2300){
+        $('#slide-3 .caption').fadeIn();
+    }else{     
+      $('#slide-3 .caption').fadeOut();
+    }
    
    });
 }]);
